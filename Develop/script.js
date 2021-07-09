8253a72cada67bb3edf2131750d3934baf08e00c
Rachel <rtc145@gmail.com>
2021-07-09 18:08:20 -0400

    API calls are working. Data can be accessed in console log

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,9 @@
 var APIKey= "eb145cf7d73a9a6af533438e5bc0b8ff";
 var search= document.getElementById("search");
 var currentCity;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=" + APIKey;
+var test= document.getElementById("test");
 
+//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 
 function searchCity(event) {
     //prevent default refresh
@@ -12,6 +13,7 @@ function searchCity(event) {
     currentCity= document.getElementById("city").value;
     //just a check to see if the above is working
     console.log(currentCity);
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&appid=" + APIKey;
     //fetch request
     fetch(queryURL)
         .then(function(response){
@@ -19,8 +21,24 @@ function searchCity(event) {
         })
         .then(function(data) {
             console.log(data);
+            console.log(data.name + "; Temperature: " + data.main.temp + " degrees Fahrenheit; Humidity: " + data.main.humidity + "%; Wind Speed: " + data.wind.speed + " mph; UV Index: ");
+            //save latitude and longitude to be used as parameters in another fetch request           
+            var lat= data.coord.lat;
+            var lon= data.coord.lon;
+            console.log(lat +" and "+ lon);
+
+            var queryURL2 = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKey;
+
+            //second fetch request
+            fetch(queryURL2)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+            });
         });
-    //call function to display search history and clear curent search
+    //call function to display search history and clear current search
 };
 
 
@@ -30,6 +48,3 @@ function searchCity(event) {
 
 //when search button is clicked...
 search.addEventListener("click", searchCity);
-
-
-
